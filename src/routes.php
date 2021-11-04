27805ca9abe0276969da5bde2306fc2d8f3c997a
Nathan Chapelle <nathanchapelle76@gmail.com>
2021-11-04 09:48:39 +0100

    erased the é in activite in  routes.php

--- src/routes.php
+++ src/routes.php
@@ -13,5 +13,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'activité' => ['ActivityController', 'show', ['id']],
+    'activite' => ['ActivityController', 'show', ['id']],
 ];
