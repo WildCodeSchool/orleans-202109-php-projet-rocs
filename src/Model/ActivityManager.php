41e9c5cd16f2248985c6dc3e66821d9775a931ee
Nathan Chapelle <nathanchapelle76@gmail.com>
2021-11-04 11:29:05 +0100

    fixed psr standart

--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -8,7 +8,8 @@ class ActivityManager extends AbstractManager
 
     public function activityById(int $id): array
     {
-        $query = 'SELECT * FROM ' . static::TABLE . ' AS a LEFT JOIN trainer AS t ON t.id = a.trainer_id WHERE a.id = :id';
+        $query = 'SELECT * FROM ' . static::TABLE .
+            ' AS a LEFT JOIN trainer AS t ON t.id = a.trainer_id WHERE a.id = :id';
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
