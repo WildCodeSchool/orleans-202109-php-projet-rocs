70f084a9ec5808f6b06bd53e8ac82beff58c5855
Bsir Rachida <roulgamous@gmail.com>
2021-11-05 10:24:55 +0100

    query

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Model\ActivityManager;
+
 class ActivityController extends AbstractController
 {
     public function index()
     {
-        return $this->twig->render('Activity/index.html.twig');
+        $activityManager = new ActivityManager();
+        $activities = $activityManager->selectAll();
+        return $this->twig->render('Activity/index.html.twig', ['activities' => $activities]);
     }
 }
