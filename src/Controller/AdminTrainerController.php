e0008b2481a20de9134142c85af0b2d707239841
Bsir Rachida <roulgamous@gmail.com>
2021-11-12 22:36:48 +0100

    ajout entraineur

---
+++ src/Controller/AdminTrainerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\TrainerManager;
+
+class AdminTrainerController extends AbstractController
+{
+    public function index(): string
+    {
+        $trainersManager = new TrainerManager();
+        $trainers = $trainersManager->adminSelectAll();
+
+        return $this->twig->render('admin/adminTrainer.html.twig', ['trainers' => $trainers]);
+    }
+
+
+    public function add(): string
+    {
+        $errors = $trainer = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $trainer = array_map('trim', $_POST);
+            if (empty($trainer['lastname'])) {
+                $errors[] = 'Le nom est obligatoire';
+            }
+            if (empty($trainer['firstname'])) {
+                $errors[] = 'Le prénom est obligatoire';
+            }
+            if (empty($trainer['phoneNumber'])) {
+                $errors[] = 'Le téléphone est obligatoire';
+            }
+            if (empty($trainer['email'])) {
+                $errors[] = 'Le mail est obligatoire';
+            } elseif (!filter_var($trainer['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors[] = 'L\'adresse mail n\'est pas au bon format';
+            }
+            if (empty($errors)) {
+                $trainersManager  = new TrainerManager();
+                $trainersManager->insert($trainer);
+                header('Location:/admin/entraineur');
+            }
+        }
+        return $this->twig->render('admin/adminAddTrainer.html.twig', ['errors' => $errors, 'trainer' => $trainer]);
+    }
+}
--- src/Model/TrainerManager.php
+++ src/Model/TrainerManager.php
@@ -5,4 +5,22 @@ namespace App\Model;
 class TrainerManager extends AbstractManager
 {
     public const TABLE = 'trainer';
+    public function adminSelectAll(): array
+    {
+        $query = "SELECT * FROM " . self::TABLE;
+        return $this->pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+
+    public function insert(array $trainer): void
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`firstname`,`lastname`,`phoneNumber`,
+        `email`,`gender`) VALUES (:firstname,:lastname,:phoneNumber,:email,:gender)");
+        $statement->bindValue('firstname', $trainer['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue('lastname', $trainer['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue('phoneNumber', $trainer['phoneNumber']);
+        $statement->bindValue('email', $trainer['email']);
+        $statement->bindValue('gender', $trainer['gender'], \PDO::PARAM_STR);
+        $statement->execute();
+    }
 }
