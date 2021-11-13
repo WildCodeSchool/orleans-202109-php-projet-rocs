5f12ed716545119428068bd9ce599d4e60c1bbd1
Bsir Rachida <roulgamous@gmail.com>
2021-11-13 07:38:02 +0100

    add-trainer

--- src/Controller/AdminTrainerController.php
+++ src/Controller/AdminTrainerController.php
@@ -15,33 +15,52 @@ class AdminTrainerController extends AbstractController
     }
 
 
+
     public function add(): string
     {
         $errors = $trainer = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
             $trainer = array_map('trim', $_POST);
+            $errors = $this->trainerValidate($trainer);
 
-            if (empty($trainer['lastname'])) {
-                $errors[] = 'Le nom est obligatoire';
-            }
-            if (empty($trainer['firstname'])) {
-                $errors[] = 'Le prénom est obligatoire';
-            }
-            if (empty($trainer['phoneNumber'])) {
-                $errors[] = 'Le téléphone est obligatoire';
-            }
-            if (empty($trainer['email'])) {
-                $errors[] = 'Le mail est obligatoire';
-            } elseif (!filter_var($trainer['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors[] = 'L\'adresse mail n\'est pas au bon format';
-            }
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
             if (empty($errors)) {
                 $trainersManager  = new TrainerManager();
                 $trainersManager->insert($trainer);
                 header('Location:/admin/entraineur');
             }
         }
+
         return $this->twig->render('admin/adminAddTrainer.html.twig', ['errors' => $errors, 'trainer' => $trainer]);
+    }
+
+    private function trainerValidate(array $trainer): array
+    {
+        $errors = [];
+        if (empty($trainer['lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        }
+        if (empty($trainer['firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        }
+        $maxfirstnameLength = 155;
+        if (strlen($trainer['firstname']) > $maxfirstnameLength) {
+            $errors[] = 'Le champ prénom ne peut être plus long que ' . $maxfirstnameLength;
+        }
+        $maxphoneNumberLength = 10;
+        if (empty($trainer['phoneNumber'])) {
+            $errors[] = 'Le téléphone est obligatoire';
+        } elseif ($trainer['phoneNumber'] > $maxphoneNumberLength || $trainer['phoneNumber'] < $maxphoneNumberLength) {
+            $errors[] = 'Téléphone invalide';
+        }
+        if (empty($trainer['email'])) {
+            $errors[] = 'Le mail est obligatoire';
+        } elseif (!filter_var($trainer['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'L\'adresse mail n\'est pas au bon format';
+        }
+        return $errors;
     }
 }
