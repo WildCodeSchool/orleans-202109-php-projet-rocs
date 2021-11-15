c7acaa3e378acb1700de4c9f89a39f97c14e7b46
CEDRIC_PARDON <cedricp02@gmail.com>
2021-11-15 13:56:32 +0100

    office delete fin

--- src/routes.php
+++ src/routes.php
@@ -9,9 +9,10 @@
 return [
     '' => ['HomeController', 'index',],
     'activites' => ['ActivityController', 'index',],
-    'admin/office' => ['AdminOfficeController', 'index'],
-    'admin/office/ajout' => ['AdminOfficeController', 'add'],
+    'admin/office' => ['AdminOfficeController', 'index',],
+    'admin/office/ajout' => ['AdminOfficeController', 'add',],
     'admin/office/editer' => ['AdminOfficeController', 'edit', ['id']],
+    'admin/office/supprimer' => ['AdminOfficeController', 'delete',],
     'association' => ['OfficeController', 'index',],
     'adhesion' => ['MembershipController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
