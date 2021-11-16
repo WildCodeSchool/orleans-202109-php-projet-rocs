b3c0d6af6afe7b14c111c88aa017424d8adc9e09
Bsir Rachida <roulgamous@gmail.com>
2021-11-16 21:32:53 +0100

    Form-trainer

--- src/Controller/AdminTrainerController.php
+++ src/Controller/AdminTrainerController.php
@@ -23,6 +23,10 @@ class AdminTrainerController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = trim($_POST['id']);
             $trainerManager = new TrainerManager();
+            $trainer = $trainerManager->selectOneById((int)$id);
+            if (file_exists('uploads/trainers' . $trainer['image'])) {
+                unlink('uploads/trainers' . $trainer['image']);
+            }
             $trainerManager->delete((int)$id);
             header('Location: /admin/entraineur');
         }
@@ -56,22 +60,22 @@ class AdminTrainerController extends AbstractController
             $errors = $this->trainerValidate($trainer);
 
             if (is_uploaded_file($_FILES['image']['tmp_name'])) {
-                $maxFileSize = '2000000';
+                $maxFileSize = '1500000';
                 if ($_FILES['image']['size'] > $maxFileSize) {
                     $errors[] = "L'image doit faire moins de " . $maxFileSize / 1000000 . "M";
                 }
-                $autorizedMimes = ['jpg', 'jpeg', 'png'];
-                $fileMime = mime_content_type($_FILES['image']['name']);
+                $autorizedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
+                $fileMime = mime_content_type($_FILES['image']['tmp_name']);
 
                 if (!in_array($fileMime, $autorizedMimes)) {
-                    $errors[] = "Veuillez sélectionner une image de type " . implode(', ', $autorizedMimes);
+                    $errors[] = "Veuillez sélectionner une image de type " . implode(' , ', $autorizedMimes);
                 }
             } else {
                 $errors[] = "Probléme de téléchargement d'image";
             }
 
             if (empty($errors)) {
-                $fileName = uniqid() . $_FILES['image']['name'];
+                $fileName = uniqid() . '_' . $_FILES['image']['name'];
                 move_uploaded_file($_FILES['image']['tmp_name'], 'uploads/trainers/' . $fileName);
                 $trainer['image'] = $fileName;
                 $trainersManager = new TrainerManager();
--- src/Model/TrainerManager.php
+++ src/Model/TrainerManager.php
@@ -10,8 +10,15 @@ class TrainerManager extends AbstractManager
     {
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
             " SET `firstname` = :firstname , `lastname` = :lastname ,
-            `phoneNumber` = :phoneNumber , `email` = :email ,`gender` = :gender,`image` = :image WHERE id=:id");
+            `phoneNumber` = :phoneNumber , `email` = :email ,`gender` = :gender WHERE id=:id");
         $statement->bindValue('id', $trainer['id'], \PDO::PARAM_INT);
+        $statement->bindValue('firstname', $trainer['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue('lastname', $trainer['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue('phoneNumber', $trainer['phoneNumber']);
+        $statement->bindValue('email', $trainer['email']);
+        $statement->bindValue('gender', $trainer['gender'], \PDO::PARAM_STR);
+
+
         return $statement->execute();
     }
 
